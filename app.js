4f17aa1abeb4c122f33ec6ba4a8cb1169ac6e953
Giovanny <ekeisco@gmail.com>
2012-03-27 10:59:23 -0500

    definiendo informacion de rutas y estaciones

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 var app = require('express').createServer();
 
 app.get('/', function(req, res){
-  res.send('Hola Transmilleno');
+  var definition = "<h1>informaci&oacute;n para rutas transmilenio</h1>"+
+  				   "<h2>Estaciones</h2>"+
+                   "<ul>"+
+                   "<li>id: </li>"+
+                   "<li>Color o sector:</li>"+
+                   "<li>coordenadas: lat y long </li>"+
+                   "<li>Direcci&oacute;n: </li>"+
+                   "<li>rutas: </li></ul>"+
+                   "<h2>Rutas</h2>"+
+                   "<ul>"+
+                   "<li>id: </li>"+
+                   "<li>Destino:</li>"+
+                   "<li>paradas: </li>"+
+                   "<li>Dias-Horarios: </li>"+
+                   "</ul>";
+  res.send(definition);
 });
 
 app.listen(3000);
