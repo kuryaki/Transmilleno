456837540b74db34167d942eb7f79b7aac2a7e5d
David Roncancio <david.roncancio@gmail.com>
2012-03-27 15:51:16 -0500

    Ajustes para la conexion a la base de datos con mongoose

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-var app = require('express').createServer();
+var application_root = __dirname,
+    express = require('express'),
+    path = require('path'),
+    mongoose = require('mongoose');
+
+var app = express.createServer();
+
+mongoose.connect('mongodb://localhost/transmilleno');
+
+app.configure(function(){
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(application_root, 'public')));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.set('views', path.join(application_root, 'views'));
+    app.set('view engine', 'jade');
+});
 
 app.get('/', function(req, res){
   var definition = "<h1>informaci&oacute;n para rutas transmilenio</h1>";
